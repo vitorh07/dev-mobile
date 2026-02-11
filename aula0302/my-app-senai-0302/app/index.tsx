4518aa68bb7ee91b098de9bc74c6acd70add7ee5
vitorh07 <vitor07devsenai@gmail.com>
2026-02-10 21:35:10 -0300

    aula10/02

--- aula0302/my-app-senai-0302/app/index.tsx
+++ aula0302/my-app-senai-0302/app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Título</Text>
+      <Text style={styles.subtitle}>Subtítulo</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#b7bdc0", // Azul claro
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 24,
+    color: "#4fbe4f",
+    fontWeight: "bold",
+  },
+});
