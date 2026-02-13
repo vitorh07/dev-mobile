ab52433f7d50819ce62a06bd13f25cf45b3a7c34
vitorh07 <vitor07devsenai@gmail.com>
2026-02-12 22:09:12 -0300

    ex1

--- aula1002/lista-exercicios/app/exercicios/ex1.tsx
+++ aula1002/lista-exercicios/app/exercicios/ex1.tsx
@@ -1,9 +1,31 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function Exercicio1() {
     return (
-        <View>
-            <Text>Exercício 1</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Meu primeiro App Expo</Text>
+            <Text style={styles.subtitle}>Construindo interfaces no React Native</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#88ceeb',
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        color: '#fff',
+        textAlign: 'center',
+    },
+    subtitle: {
+        fontSize: 22,
+        color: '#fff',
+        textAlign: 'center',
+    },
+});
