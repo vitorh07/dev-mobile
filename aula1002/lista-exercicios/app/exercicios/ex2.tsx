04fa41bf4430110e58f1e171c0cb2b7afed88c9f
vitorh07 <vitorfogaca0227@gmail.com>
2026-02-23 16:51:03 -0300

    ex2

--- aula1002/lista-exercicios/app/exercicios/ex2.tsx
+++ aula1002/lista-exercicios/app/exercicios/ex2.tsx
@@ -1,12 +1,20 @@
-
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 export default function Exercicio2() {
     return (
         <View style={styles.container}>
             <View style={styles.box}>
+                <Image
+                    source={{
+                        uri: 'https://yt3.googleusercontent.com/-_svtX8lvgU1y99-7_U2vfsZN96slULOehNKGumhzlvy18cE_DCq7XS6br_oPb-MQezXPGTC=s900-c-k-c0x00ffffff-no-rj'
+                    }}
+                    style={styles.roundImage}
+                />
+
                 <Text style={styles.nome}>Carlos Silva</Text>
-                <Text style={styles.texto}>Desenvolvedor Mobile | React Native</Text>
+                <Text style={styles.texto}>
+                    Desenvolvedor Mobile | React Native
+                </Text>
             </View>
         </View>
     );
@@ -15,29 +23,48 @@ export default function Exercicio2() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#f0f0f0',
+        padding: 20,
     },
+
     box: {
-        width: '25%',
-        height: '40%',
+        width: 300,
         backgroundColor: '#ffffff',
         borderRadius: 18,
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        paddingVertical: 30,
+        paddingHorizontal: 20,
+        alignItems: 'center',
+
+        // Android
+        elevation: 5,
+
+        // iOS
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.1,
+        shadowRadius: 8,
     },
+
+    roundImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        marginBottom: 20,
+    },
+
     nome: {
-        fontSize: 24,
+        fontSize: 22,
         fontWeight: 'bold',
-        color: '#000000',
+        color: '#000',
         textAlign: 'center',
-        marginTop: 20,
+        marginBottom: 8,
     },
+
     texto: {
         fontSize: 16,
-        color: '#0e0e0e',
+        color: '#555',
         textAlign: 'center',
-        marginTop: 10,
-    },  
+    },
 });
